a2471f689096128813fffa7368e0ba7364d33b47
Rakib Hossain <rakibrk1811@gmail.com>
2024-10-19 14:39:45 +0530

    adding proxy design patterns

---
+++ patterns/structural/Proxy.java
@@ -0,0 +1,48 @@
+package patterns.structural;
+
+/*
+ * Proxy: provides a placeholder or intermediary object to control access to another object
+ */
+
+interface Account {
+    public void showBalance();
+    public void withdrawBalance(int amount);
+}
+
+class BankAccount implements Account {
+    @Override
+    public void showBalance() {
+        
+    }
+
+    @Override
+    public void withdrawBalance(int amount) {
+        
+    }
+}
+
+// ATM is a proxy to BankAccount, so access can be control from here
+class ATM implements Account {
+    private BankAccount bankAccount;
+
+    ATM() {
+        this.bankAccount = new BankAccount();
+    }
+
+    @Override
+    public void showBalance() {
+        this.bankAccount.showBalance();        
+    }
+
+    @Override
+    public void withdrawBalance(int amount) {
+        this.bankAccount.withdrawBalance(amount);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.showBalance();
+    }
+}
